7cc382168993713e0e79216cc3ff3cfdf1c611f4
Songsangsung <21700383@handong.ac.kr>
2023-08-02 00:11:59 +0900

    added todolist feature

--- OpenAPI_project/js/todo.js
+++ OpenAPI_project/js/todo.js
@@ -2,8 +2,52 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //for each toDo in toDos keep toDo if id is not equal to li's id
+    saveToDos(); // finally save new array
+}
+
+function paintToDo(newTodoObj){
+    const li = document.createElement("li");
+    li.id = newTodoObj.id;
+    const span = document.createElement("span");
+    span.innerText = newTodoObj.text; // newTodoObj is an object that has text variable
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
 function handleTodoSubmit(event){
-    event.
+    event.preventDefault();
+    const newTodo = toDoInput.value; //copy current value to variable
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now() //serve as random number for id
+    }
+    toDoInput.value = ""; //empty value
+    toDos.push(newTodoObj); // pushing newTodo to array
+    paintToDo(newTodoObj); 
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos; // restore the old toDos
+    parsedToDos.forEach(paintToDo);
+}
