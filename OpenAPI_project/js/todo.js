0e9dce073fd0b236cad59e7f7a350c7fb3f1a406
Songsangsung <21700383@handong.ac.kr>
2023-08-01 14:44:30 +0900

    Login, Clock, Background from Nomad Example

---
+++ OpenAPI_project/js/todo.js
@@ -0,0 +1,9 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function handleTodoSubmit(event){
+    event.
+}
+
+toDoForm.addEventListener("submit", handleTodoSubmit);
