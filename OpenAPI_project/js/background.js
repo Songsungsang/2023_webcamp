3455784705535272a6aa62d0cd09670a88d1c386
Songsangsung <21700383@handong.ac.kr>
2023-08-02 15:40:40 +0900

    Add background

--- OpenAPI_project/js/background.js
+++ OpenAPI_project/js/background.js
@@ -2,8 +2,9 @@ const images =["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+//const bgImage = document.createElement("img");
+//bgImage.src = `./img/${chosenImage}`;
+//document.body.appendChild(bgImage);
 
-bgImage.src = `./img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+const bgImage = `url('./img/${chosenImage}')`; //src
+document.body.style.backgroundImage=bgImage;
